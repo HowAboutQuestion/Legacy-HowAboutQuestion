21f584d73d0ff2e52884ccda7c6322c27fd6422e
haerim-kweon <96405505+haerim-kweon@users.noreply.github.com>
2024-12-10 21:00:34 +0900

    init

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+class Dashboard extends Component {
+  
+    render(){
+      return (
+        <div>
+          하잉하이앙히ㅏ이
+        </div>
+         
+  
+      );
+    }
+}
+
+export default Dashboard;
