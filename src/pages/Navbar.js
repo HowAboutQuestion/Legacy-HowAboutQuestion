b02e39d4fcc13abcbcd5f21c7b34e7d46b99d4b2
haerim-kweon <96405505+haerim-kweon@users.noreply.github.com>
2024-12-13 20:18:17 +0900

    feature: add question, tag filter

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -13,17 +13,17 @@ function Navbar() {
         <div className="h-full px-3 py-4 border-r ">
            <ul className="space-y-2 font-medium">
               <li>
-                 <div onClick={goDashBoard} className="flex justify-center items-center p-2 rounded-lg text-white hover:bg-gray-100 group">
-                    <svg className="w-5 h-5 transition duration-75 text-gray-400 group-hover:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                 <div onClick={goDashBoard} className="flex justify-center items-center p-2 rounded-lg text-white hover:bg-blue-50 group">
+                    <svg className="w-5 h-5 transition duration-75 text-blue-400 group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
                     </svg>
                  </div>
               </li>
               <li>
-                 <div onClick={goQuestions} className="flex justify-center items-center p-2 rounded-lg text-white hover:bg-gray-100 group">
+                 <div onClick={goQuestions} className="flex justify-center items-center p-2 rounded-lg text-white hover:bg-blue-50 group">
                     <svg 
-                    className="flex-shrink-0 w-5 h-5 transition duration-75 text-gray-400 group-hover:text-gray-500" aria-hidden="true" 
+                    className="flex-shrink-0 w-5 h-5 transition duration-75 text-blue-400 group-hover:text-blue-500" aria-hidden="true" 
                     xmlns="http://www.w3.org/2000/svg" 
                     fill="currentColor" 
                     viewBox="0 0 18 18">
@@ -33,12 +33,12 @@ function Navbar() {
                  </div>
               </li>
               <li>
-                 <div onClick={goSolve} className="flex justify-center items-center p-2 rounded-lg text-white hover:bg-gray-100 group">
+                 <div onClick={goSolve} className="flex justify-center items-center p-2 rounded-lg text-white hover:bg-blue-50 group">
                   <svg
                      xmlns="http://www.w3.org/2000/svg"
                      viewBox="0 0 24 24"
                      fill="currentColor"
-                     className="flex-shrink-0 size-6 transition duration-75 text-gray-400 group-hover:text-gray-500" aria-hidden="true" 
+                     className="flex-shrink-0 size-6 transition duration-75 text-blue-400 group-hover:text-blue-500" aria-hidden="true" 
                      >
                      <path d="M11.7 2.805a.75.75 0 0 1 .6 0A60.65 60.65 0 0 1 22.83 8.72a.75.75 0 0 1-.231 1.337 49.948 49.948 0 0 0-9.902 3.912l-.003.002c-.114.06-.227.119-.34.18a.75.75 0 0 1-.707 0A50.88 50.88 0 0 0 7.5 12.173v-.224c0-.131.067-.248.172-.311a54.615 54.615 0 0 1 4.653-2.52.75.75 0 0 0-.65-1.352 56.123 56.123 0 0 0-4.78 2.589 1.858 1.858 0 0 0-.859 1.228 49.803 49.803 0 0 0-4.634-1.527.75.75 0 0 1-.231-1.337A60.653 60.653 0 0 1 11.7 2.805Z" />
                      <path d="M13.06 15.473a48.45 48.45 0 0 1 7.666-3.282c.134 1.414.22 2.843.255 4.284a.75.75 0 0 1-.46.711 47.87 47.87 0 0 0-8.105 4.342.75.75 0 0 1-.832 0 47.87 47.87 0 0 0-8.104-4.342.75.75 0 0 1-.461-.71c.035-1.442.121-2.87.255-4.286.921.304 1.83.634 2.726.99v1.27a1.5 1.5 0 0 0-.14 2.508c-.09.38-.222.753-.397 1.11.452.213.901.434 1.346.66a6.727 6.727 0 0 0 .551-1.607 1.5 1.5 0 0 0 .14-2.67v-.645a48.549 48.549 0 0 1 3.44 1.667 2.25 2.25 0 0 0 2.12 0Z" />
