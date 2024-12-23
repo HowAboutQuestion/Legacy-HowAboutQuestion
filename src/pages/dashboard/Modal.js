94f18c8bc4c489d1d78487f2fadbb55b4f17d358
cod0216 <cod0216@nate.com>
2024-12-23 10:08:46 +0900

    mdofiy: modal

--- src/pages/dashboard/Modal.js
+++ src/pages/dashboard/Modal.js
@@ -4,17 +4,11 @@ import ReactDOM from 'react-dom';
 const Modal = ({ children, onClose }) => {
   return ReactDOM.createPortal(
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white rounded-lg shadow-lg relative">
-        <button 
-          onClick={onClose} 
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-        >
-          &times;
-        </button>
+      <div className="bg-white rounded-lg shadow-lg relative w-full max-w-lg p-6">
         {children}
       </div>
     </div>,
-    document.getElementById('modal-root') // index.html에 modal-root가 존재해야 합니다.
+    document.getElementById('modal-root')
   );
 };
 
