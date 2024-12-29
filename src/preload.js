8d1b46ebc35c731b9a4ccdd106b89c2f323d8418
haerim-kweon <96405505+haerim-kweon@users.noreply.github.com>
2024-12-29 17:59:11 +0900

    add .env : file path

--- src/preload.js
+++ src/preload.js
@@ -17,10 +17,32 @@ contextBridge.exposeInMainWorld('electronAPI', {
   },
 
   updateQuestions: (questions) => {
-    return ipcRenderer.invoke('update-questions', questions)
+    return ipcRenderer.invoke('update-questions-file', questions)
   },
 
   //.zip 내보내기
   exportQuestions: (questions) => ipcRenderer.invoke("export-questions", questions),
+
+  //.zip 
+  extractZip: async (file) => {
+    const reader = new FileReader();
+    return new Promise((resolve, reject) => {
+      reader.onload = () => {
+        const arrayBuffer = reader.result;
+        ipcRenderer
+          .invoke("extract-zip", {
+            fileName: file.name, // 파일 이름 전달
+            content: Buffer.from(arrayBuffer), // ArrayBuffer를 Buffer로 변환
+          })
+          .then(resolve)
+          .catch(reject);
+      };
+      reader.onerror = () => reject(reader.error);
+      reader.readAsArrayBuffer(file); // File 객체를 ArrayBuffer로 읽기
+    });
+  },
+
+  readQuestionsCSV: () => ipcRenderer.invoke('read-questions-csv'),
+
 
 });
