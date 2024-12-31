fc78f1e2959f383e65d8e50a474ccecb8c2269e6
cod0216 <cod0216@nate.com>
2025-01-01 04:32:04 +0900

    feat: recommand Questions select solve

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,19 +1,39 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
+import { selectedTagsAtom, selectedQuestionsAtom } from 'state/data';
 
 
 function Navbar() {
    const navigate = useNavigate();
-   const goDashBoard = () => {navigate("/dashboard")}
-   const goQuestions = () => {navigate("/questions")}
-   const goSolve = () => {navigate("/select")}
+   const location = useLocation();
+   const setSelectedTags = useSetRecoilState(selectedTagsAtom);
+   const setSelectedQuestions = useSetRecoilState(selectedQuestionsAtom);
+ 
+   // 네비게이션과 아톰 초기화를 함께 수행하는 헬퍼 함수
+   const handleNavigation = (path) => {
+     // 선택된 태그와 문제 초기화
+     setSelectedTags([]);
+     setSelectedQuestions([]);
+     // 원하는 경로로 네비게이션
+     navigate(path);
+   };
+
+  const goDashboard = () => handleNavigation("/dashboard");
+  const goQuestions = () => handleNavigation("/questions");
+  const goSolve = () => {
+   if (location.pathname !== "/select") {
+     handleNavigation("/select");
+   }
+   // 현재 경로가 "/select"일 경우 아무 것도 하지 않음
+ };
   
    return (
         <aside className="fixed top-0 left-0 z-40 w-20 h-full shadow-xl " >
         <div className="h-full px-3 py-4 border-r ">
            <ul className="space-y-2 font-medium">
               <li>
-                 <div onClick={goDashBoard} className="flex justify-center items-center p-2 rounded-lg text-white hover:bg-blue-50 group">
+                 <div onClick={goDashboard} className="flex justify-center items-center p-2 rounded-lg text-white hover:bg-blue-50 group">
                     <svg className="w-5 h-5 transition duration-75 text-blue-400 group-hover:text-blue-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
