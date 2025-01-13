9d313b973e10d89096f9c06339fc517f6176ee03
haelim-kweon <khaelim1311@gmail.com>
2025-01-13 12:52:16 +0900

    feat: add util koreaTime

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,5 +1,13 @@
 import { format } from 'date-fns';
 
+export const getTodayDate = () => {
+  const offset = 1000 * 60 * 60 * 9;
+  const koreaTime = new Date((new Date()).getTime() + offset).toISOString().split("T")[0];
+  
+  
+  return koreaTime;
+}
+
 export const formatDate = (date) => {
   return format(date, 'yyyy-MM-dd');
 };
