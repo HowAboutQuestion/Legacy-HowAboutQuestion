45aade659a2e910bebb260b515cea0eb87f24084
cod0216 <cod0216@nate.com>
2025-01-21 23:39:38 +0900

    feat: deploy

--- src/preload.js
+++ src/preload.js
@@ -1,23 +1,18 @@
 const { contextBridge, ipcRenderer } = require('electron');
+
 console.log('Preload script loaded successfully');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-
   updateHistory: (data) => ipcRenderer.invoke('update-history', data),
   saveImage: async (id, file) => {
     const reader = new FileReader();
     return new Promise((resolve, reject) => {
       reader.onload = () => {
         const arrayBuffer = reader.result;
-
-        // 파일 확장자
         const fileExtension = file.name.split('.').pop();
-        const fileName = `${id}.${fileExtension}`; // id 기반 파일 이름 생성
-
-
-
+        const fileName = `${id}.${fileExtension}`;
         ipcRenderer
-          .invoke('save-image', {fileName, content: Buffer.from(arrayBuffer) })
+          .invoke('save-image', { fileName, content: Buffer.from(arrayBuffer) })
           .then(resolve)
           .catch(reject);
       };
@@ -25,24 +20,12 @@ contextBridge.exposeInMainWorld('electronAPI', {
       reader.readAsArrayBuffer(file);
     });
   },
-
-  deleteImage: (imgPath) => {
-    //console.log("deleteImage called with imgPath: ", imgPath);
-    return ipcRenderer.invoke('delete-image', { imgPath });
-  },
-
-    updateQuestion: async ({ title, type, isCorrect }) => {
+  deleteImage: (imgPath) => ipcRenderer.invoke('delete-image', { imgPath }),
+  updateQuestion: async ({ title, type, isCorrect }) => {
     return ipcRenderer.invoke('update-question', { title, type, isCorrect });
   },
-
-  updateQuestions: (questions) => {
-    return ipcRenderer.invoke('update-questions-file', questions)
-  },
-
-  //.zip 내보내기
+  updateQuestions: (questions) => ipcRenderer.invoke('update-questions-file', questions),
   exportQuestions: (questions) => ipcRenderer.invoke("export-questions", questions),
-
-  //.zip 
   extractZip: async (file) => {
     const reader = new FileReader();
     return new Promise((resolve, reject) => {
@@ -50,19 +33,16 @@ contextBridge.exposeInMainWorld('electronAPI', {
         const arrayBuffer = reader.result;
         ipcRenderer
           .invoke("extract-zip", {
-            fileName: file.name, // 파일 이름 전달
-            content: Buffer.from(arrayBuffer), // ArrayBuffer를 Buffer로 변환
+            fileName: file.name,
+            content: Buffer.from(arrayBuffer),
           })
           .then(resolve)
           .catch(reject);
       };
       reader.onerror = () => reject(reader.error);
-      reader.readAsArrayBuffer(file); // File 객체를 ArrayBuffer로 읽기
+      reader.readAsArrayBuffer(file);
     });
   },
-
   readQuestionsCSV: () => ipcRenderer.invoke('read-questions-csv'),
-  readHistoryCSV: () => ipcRenderer.invoke('read-history-csv'), 
-
-
+  readHistoryCSV: () => ipcRenderer.invoke('read-history-csv'),
 });
