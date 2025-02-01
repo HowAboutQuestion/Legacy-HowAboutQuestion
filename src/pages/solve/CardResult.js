c156a380e6bb25995f912a41c2694d4807e1b8ae
haelim-kweon <khaelim1311@gmail.com>
2025-02-01 23:58:26 +0900

    feat: card button

--- src/pages/solve/CardResult.js
+++ src/pages/solve/CardResult.js
@@ -64,7 +64,7 @@ function CardResult() {
               <div className="mt-2 h-1/2">
                   <Doughnut data={data} options={options} />
               </div>
-              <div className="flex justify-between w-100 px-5">
+              <div className="flex justify-between w-full px-5">
                 <div 
                 onClick={goCard}
                 className="bg-blue-500 rounded-2xl py-2 w-20 whitespace-nowrap text-white font-bold text-xs text-center hover:shadow hover:scale-105">다시풀기</div>
