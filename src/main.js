2faa48606271be51b44cf1837c12f2d2b3f62569
haelim-kweon <khaelim1311@gmail.com>
2025-03-04 12:29:42 +0900

    feat: 결과 확인 페이지에 문제 설명란 추가 및 pdf 변환

--- src/main.js
+++ src/main.js
@@ -185,7 +185,9 @@ function updateHistory(isCorrect) {
 }
 
 
-
+/**
+ * 현재 사용하는 메소드인지 확인 필요요
+ */
 function updateQuestion(title, type, isCorrect) {
   try {
     const csvPath = questionsCsvPath;
@@ -531,6 +533,7 @@ ipcMain.handle('extract-zip', async (event, { fileName, content }) => {
           select2: item.select2 || '',
           select3: item.select3 || '',
           select4: item.select4 || '',
+          description : item.description || '',
           answer: item.answer || '',
           img: item.img || '',
           level: 0,
@@ -560,8 +563,6 @@ ipcMain.handle('extract-zip', async (event, { fileName, content }) => {
   }
   return result;
 });
-
-
 
 ipcMain.handle('delete-image', async (event, { imgPath }) => {
   console.log("delete-image imgPath: ", imgPath);
