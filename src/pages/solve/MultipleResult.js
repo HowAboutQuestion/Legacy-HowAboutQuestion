0a6045d3b6648742ee3aca6e5ad0396f7abbdec0
haelim-kweon <khaelim1311@gmail.com>
2025-03-04 12:29:42 +0900

    feat: 결과 확인 페이지에 문제 설명란 추가 및 pdf 변환

--- src/pages/solve/MultipleResult.js
+++ src/pages/solve/MultipleResult.js
@@ -9,6 +9,11 @@ function MultipleResult({ question, index }) {
   const [showModal, setShowModal] = useState(false);
   const openModal = () => setShowModal(true);
   const closeModal = () => setShowModal(false);
+  const [showDescription, setShowDescription] = useState(false);
+  const tags = question.tag || [];
+  const tag = tags.map((tagName, index) => <div key={index} className="font-medium text-xs whitespace-nowrap bg-gray-200 rounded-xl py-1 px-2">{tagName}</div>);
+
+
 
   const Modal = ({ imgSrc, onClose }) => {
     return (
@@ -27,27 +32,32 @@ function MultipleResult({ question, index }) {
   };
 
   return (
-    <div className="flex flex-col gap-5 p-10">
-      <div className="text-lg font-bold">
-        <span>{index + 1}.</span>
+  <div className="flex flex-col gap-3 p-10 items-start w-full">
+      {/* 문제 제목 */} 
+      <div className="text-lg font-bold w-full text-start">
+        <span>{index + 1}. </span>
         <span>{question.title}</span>
       </div>
 
-      <div className="flex gap-3">
+      {/* 문제 태그 */}
+      {tag.length > 0 && (<div className="flex gap-1 justify-start text-start">{tag}</div>)}
 
-      
+      <div className="flex gap-3 flex-col w-full">
+
+      {/* 문제 이미지지 */}      
       {question.img && (
         <div>
           <img
             onClick={openModal}
-            className="bg-gray-50 max-w-max w-60 h-auto rounded"
+            className="bg-gray-50 w-full h-auto rounded"
             src={appPath + question.img}
             alt=""
           />
         </div>
       )}
 
-      <div className="font-normal text-sm flex flex-col gap-2 w-max">     
+      {/* 사용자 입력 답안 / 문제 정답 */}
+      <div className="font-normal text-sm flex flex-col gap-2 w-full text-center">     
         {question.select1 && (<div className={`box border rounded-lg p-2 px-5
               ${
                 question.answer === question.select1 ? "text-blue-500 font-bold" : question.selected === question.select1 ? "text-red-500 font-bold" : "" 
@@ -84,6 +94,33 @@ function MultipleResult({ question, index }) {
           </div>)}
                  
       </div>
+     
+     
+      {/* 문제 해설 */}
+      {question.description && (
+          <div className="relative w-full">
+          
+        <div
+          className={`mt-10 rounded-xl p-5 font-md bg-gray-100 w-full overflow-hidden text-clip transition-all inline-flex gap-1 ${
+            !showDescription ? "blur-sm" : "blur-none"
+          }`}>
+          <img src="./images/light_icon.png" 
+            onClick={() => setShowDescription(false)}
+            className="w-5 h-5 cursor-pointer hover:scale-110 transition-all text"/>
+            {question.description}
+        </div>
+        {!showDescription && (
+            <button
+              className="mt-10 absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 text-white font-bold rounded-xl"
+              onClick={() => setShowDescription(true)}
+            >
+              해설보기
+            </button>
+          )}
+        </div>
+      
+      )}
+  
       </div>
       {showModal && <Modal imgSrc={appPath + question.img} onClose={closeModal} />}
     </div>
--- src/pages/solve/SingleResult.js
+++ src/pages/solve/SingleResult.js
@@ -8,6 +8,10 @@ function SingleResult({ question, index }) {
   const [showModal, setShowModal] = useState(false);
   const openModal = () => setShowModal(true);
   const closeModal = () => setShowModal(false);
+  const [showDescription, setShowDescription] = useState(false);
+  const tags = question.tag || [];
+  const tag = tags.map((tagName, index) => <div key={index} className="font-medium text-xs whitespace-nowrap bg-gray-200 rounded-xl py-1 px-2">{tagName}</div>);
+
 
   const Modal = ({ imgSrc, onClose}) => (
     <div
@@ -24,34 +28,67 @@ function SingleResult({ question, index }) {
   );
 
   return (
-    <div className="flex flex-col gap-3 p-10 ">
-      <div className="text-lg font-bold">
-        <span>{index + 1}.</span>
+    <div className="flex flex-col gap-3 p-10 items-start w-full">
+    {/* 문제 제목 */}
+      <div className="text-lg font-bold w-full text-start">
+        <span>{index + 1}. </span>
         <span>{question.title}</span>
       </div>
+      {/* 문제 태그 */}
+      {tag.length > 0 && (<div className="flex gap-1 justify-start text-start">{tag}</div>)}
+
+      {/* 문제 이미지 */}
       {question.img && (
         <div>
           <img
             onClick={openModal}
-            className="bg-gray-50 max-w-max w-96 h-auto"
+            className="bg-gray-50 w-full rounded h-auto"
             src={appPath + question.img}
             alt=""
           />
         </div>
       )}
 
-      <div className="text-sm w-max"> 
-      <div className={`box border font-bold rounded-lg p-2 px-5 
-          ${question.answer === question.selected ? "text-blue-500" : "text-red-500"}
-          `}>
-          {question.selected ? question.selected : "답변안함"}
-        </div>
+      {/* 사용자 입력 답안 / 문제 정답 */}
+      <div className="text-sm w-full"> 
+        <div className={`box border font-bold rounded-lg p-2 px-5 text-center 
+            ${question.answer === question.selected ? "text-blue-500" : "text-red-500"}
+            `}>
+            {question.selected ? question.selected : "답변안함"}
+          </div>
 
-        <div className="box text-blue-500 font-bold border rounded-lg p-2 px-5 mt-2">
-          {question.answer}
+          <div className="box text-blue-500 font-bold border rounded-lg p-2 px-5 mt-2 text-center">
+            {question.answer}
+          </div>
+      </div>
+
+      {/* 문제 해설 */}
+      {question.description && (
+        <div className="relative w-full">
+          
+          <div
+          className={`mt-10 rounded-xl p-5 font-md bg-gray-100 w-full overflow-hidden text-clip transition-all inline-flex gap-1 ${
+            !showDescription ? "blur-sm" : "blur-none"
+          }`}>
+          <img src="./images/light_icon.png" 
+            onClick={() => setShowDescription(false)}
+            className="w-5 h-5 cursor-pointer hover:scale-110 transition-all text"/>
+            {question.description}
+          </div>
+          {!showDescription && (
+            <button
+              className="mt-10 absolute inset-0 flex items-center justify-center bg-black bg-opacity-40 text-white font-bold rounded-xl"
+              onClick={() => setShowDescription(true)}
+            >
+              해설보기
+            </button>
+          )}
         </div>
-        </div>
+      )}
+      
       {showModal && <Modal imgSrc={appPath + question.img} onClose={closeModal} />}
+
+
     </div>
   );
 }
