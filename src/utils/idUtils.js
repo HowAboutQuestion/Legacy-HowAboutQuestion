2342e3f5b733cb1b2b74eb4edea06226647605b2
haelim-kweon <khaelim1311@gmail.com>
2025-04-20 08:28:06 +0900

    feat: build check

--- src/utils/idUtils.js
+++ src/utils/idUtils.js
@@ -1,14 +1,19 @@
 function generateUniqueId(questions) {
     const generateRandomId = () => {
-        return `id-${Math.random().toString(36).slice(2, 11)}`;
+      return `id-${Math.random().toString(36).slice(2, 11)}`;
     };
-
+  
     let newId;
     do {
-        newId = generateRandomId();
-    } while (Array.isArray(questions) && questions.some(question => question?.id === newId));
-
+      newId = generateRandomId();
+    } while (
+      Array.isArray(questions) &&
+      questions.some((question) => question?.id === newId)
+    );
+  
     return newId;
-}
-
-export { generateUniqueId };
+  }
+  
+  module.exports = {
+    generateUniqueId
+  };
