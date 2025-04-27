a5d12dd430a0c27e61ff268cb66eb5b5278dbd03
haelim-kweon <khaelim1311@gmail.com>
2025-04-27 14:20:04 +0900

    modify: change paths from CommonJS to ESM

--- src/config/paths.js
+++ src/config/paths.js
@@ -1,7 +1,13 @@
-const path = require('path');
-const { app } = require('electron');
+import path from 'path';
+import { app } from 'electron';
 
-// 경로 설정
+/**
+ * @fileoverview
+ * 이 모듈은 Electron 애플리케이션의 경로와 관련된 설정을 다룹니다.
+ * 애플리케이션의 실행 디렉토리, 사용자 데이터 경로, CSV 파일 경로, 이미지 디렉토리 등을 설정하여
+ * 다양한 경로를 가져올 수 있도록 합니다.
+ */
+
 const exeDir = path.dirname(app.getPath('exe'));
 const userDataPath = app.getPath('userData');
 const questionsCsvPath = path.join(userDataPath, 'questions.csv');
@@ -12,7 +18,7 @@ const tempDir = path.join(app.getPath('temp'), 'questions_export');
 console.log('questionsCsvPath:', questionsCsvPath);
 console.log('historyCsvPath:', historyCsvPath);
 
-module.exports = {
+export {
   exeDir,
   userDataPath,
   questionsCsvPath,
