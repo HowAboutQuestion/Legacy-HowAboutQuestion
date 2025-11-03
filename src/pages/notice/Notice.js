da430aa46dffdbb59123d3c29f5d36770708e563
cod0216 <cod0216@nate.com>
2025-11-03 15:37:11 +0900

    modify: underline color modify and add patch url

--- src/pages/notice/Notice.js
+++ src/pages/notice/Notice.js
@@ -21,6 +21,27 @@ const Notice = ({ closeNotice }) => {
         <div className= "flex justify-center items-center">
           <PatchNotes130/>
         </div>
+        <div className="flex justify-end px-8 pb-4">
+          <a
+            href="https://github.com/HowAboutQuestion/Lagacy-HowAboutQuestion/releases"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-1 text-sm font-semibold text-gray-400 hover:underline transition-all"
+          >
+            지난 패치 보러 가기
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              stroke="currentColor"
+              className="w-4 h-4 transition-transform group-hover:translate-x-1"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+            </svg>
+          </a>
+        </div>
+
         <div className=" mt-4">
           
         </div>
