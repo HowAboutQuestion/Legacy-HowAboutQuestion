5488d3e90828ab96cc15604ce6d627caa9970c7d
cod0216 <cod0216@nate.com>
2026-06-24 16:21:32 +0900

    docs: update notice

---
+++ src/pages/notice/patchNote/patchNotes_1.4.0.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function PatchNotes140() {
+  return (
+    <div className="w-full">
+      <header className="mt-8 relative w-full h-64 overflow-hidden bg-white">
+        <a
+          href="https://howaboutquestion.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="absolute inset-0"
+        >
+          <img
+            src="./images/help/1_4_0/os.png"
+            alt="랜딩 페이지"
+            className="w-full h-full object-contain cursor-pointer"
+          />
+        </a>
+      </header>
+      
+    </div>
+  );
+}
+
+export default PatchNotes140;
